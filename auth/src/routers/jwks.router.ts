d526c2ae2e9444677daec87b05c6ffae86b964fb
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2026-03-18 03:45:22 +0530

    🎨 start implementing v2 of mint application (#4)
    * :fire: remove current implementation
    
    * ✨ implement base auth service using fastauth (#3)
    
    * :sparkles: impl auth service with fastapi and fastauth
    
    * :bricks: setup docker and dockerignore
    
    * :bug: fix keys error within the container
    
    * :wrench: setup migrations using alembic
    
    * :memo: update README.md

--- auth/src/routers/jwks.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as jwksController from "~/controllers/jwks.controller";
-
-const jwksRouter = Router();
-
-jwksRouter.route("/jwks.json").get(jwksController.getJWKS);
-
-export { jwksRouter };
--- wallet/src/schemas/domain/transaction.domain.schema.ts
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-
-export const transactionType = z.enum(["TopUp", "Transfer"]);
-export const transactionStatus = z.enum(["Pending", "Processing", "Completed", "Failed"]);
